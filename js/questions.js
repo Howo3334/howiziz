44f13b25b312ba03378b6984766051231e62de35
Howo3334 <129414253+Howo3334@users.noreply.github.com>
2023-03-30 18:37:27 +0300

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,182 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "We went___at the wekend?",
+    answer: "shoping",
+    options: [
+      "the shops",
+      "shoping",
+      "to shops",
+      "shops"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What____to night?",
+    answer: "are they doing",
+    options: [
+      "they do",
+      "do they do",
+      "are they doing",
+      "are they do"
+    ]
+  },
+    {
+    numb: 3,
+    question: "We go shoping____mont?",
+    answer: "twice",
+    options: [
+      "every a",
+      "every to",
+      "twice",
+      "once"
+    ]
+  },
+    {
+    numb: 4,
+    question: "My birthday is on February____?",
+    answer: "10th",
+    options: [
+      "10rd",
+      "10st",
+      "10th",
+      "10nd"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What____your father look like",
+    answer: "does",
+    options: [
+      "Is",
+      "are",
+      "do",
+      "does"
+    ]
+  },
+   {
+    numb: 6,
+    question: "____You like a coffee?",
+    answer: "would",
+    options: [
+      "will",
+      "did",
+      "would",
+      "do"
+    ]
+   },
+   {
+    numb: 7,
+    question: "We___born 1985",
+    answer: "were",
+    options: [
+      "is",
+      "was",
+      "were",
+      "did"
+    ]
+   },
+   {
+    numb: 8,
+    question: "____She like sport?",
+    answer: "does",
+    options: [
+      "do",
+      "is",
+      "does",
+      "dont"
+    ]
+   },
+   {
+    numb: 9,
+    question: "Do you like____DVDs",
+    answer: "watching",
+    options: [
+      "watch",
+      "watching",
+      "to watching",
+      "watches"
+    ]
+   },
+   {
+    numb: 10,
+    question: "I____a student of English",
+    answer: "am",
+    options: [
+      "am",
+      "are",
+      "aren't",
+      "is"
+    ]
+   },
+   {
+    numb: 11,
+    question: "When___have lunch",
+    answer: "does he",
+    options: [
+      "is he",
+      "do he",
+      "hes",
+      "does he"
+    ]
+   },
+   {
+    numb: 12,
+    question: "They___their homework",
+    answer: "are doing",
+    options: [
+      "are do",
+      "are doing",
+      "did",
+      "does"
+    ]
+   },
+   {
+    numb: 13,
+    question: "The cinema is___the bank",
+    answer: "opposite",
+    options: [
+      "next",
+      "in front",
+      "under",
+      "opposite"
+    ]
+   },
+   {
+    numb: 14,
+    question: "They____fly to India",
+    answer: "didn't",
+    options: [
+      "didn't",
+      "haven't",
+      "doesn't",
+      "has"
+    ]
+   },
+   {
+    numb: 15,
+    question: "____you spoken to Janny",
+    answer: "did",
+    options: [
+      "did",
+      "have",
+      "do",
+      "has"
+    ]
+   }
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
